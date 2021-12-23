fc28580b0b6f175f6d3738d0d373a8c0ae14567b
yucboy96 <944919352@qq.com>
2021-12-23 21:04:17 +0800

    add labEvents

--- controllers/service.js
+++ controllers/service.js
@@ -29,16 +29,43 @@ let allServices = {
         })
 
     },
-   getHADM_ID: function (subjectID) {
-       console.log(subjectID);
+    getAllSubject:function(){
+        let _sql = `select SUBJECT_ID from PATIENTS`
+        return allServices.query(_sql)
+    },
+    /*
+    getHADM_ID: function (subjectID) {
+        console.log(subjectID);
         let _sql = `select SUBJECT_ID,HADM_ID from ADMISSIONS where SUBJECT_ID=?;`
         return allServices.query(_sql,[subjectID])
     },
-   getAllSubject:function(){
-    let _sql = `select SUBJECT_ID from PATIENTS`
-    return allServices.query(_sql)
-   }
-    
+    */
+    getPatientInfo: function (subjectID) {
+        let _sql = `select SUBJECT_ID,GENDER,DOB from PATIENTS where SUBJECT_ID=?;`
+        return allServices.query(_sql,[subjectID])
+    },
+    getAdmissions: function (subjectID) {
+        let _sql = `select SUBJECT_ID,HADM_ID,ADMITTIME,DISCHTIME from ADMISSIONS where SUBJECT_ID=?;`
+        return allServices.query(_sql,[subjectID])
+    },
+    getPrescriptions: function (subjectID) {
+        let _sql = `select SUBJECT_ID,HADM_ID,STARTDATE,ENDDATE,DRUG,GSN,PROD_STRENGTH,DOSE_VAL_RX,DOSE_UNIT_RX from PRESCRIPTIONS where SUBJECT_ID=?;`
+        return allServices.query(_sql,[subjectID])
+    },
+    getLabEvents: function ({subjectID, hadmID}){
+        let _sql = `select * from LABEVENTS where SUBJECT_ID=?`
+        if(typeof hadmID === 'number') _sql += ' and HADM_ID=?'
+        return allServices.query(_sql,[subjectID,hadmID])
+    },
+    getChartEvents:function ({subjectID, hadmID}){
+        let _sql = `select * from CHARTEVENTS where SUBJECT_ID=?`
+        if(typeof hadmID === 'number') _sql += ' and HADM_ID=?'
+        return allServices.query(_sql,[subjectID,hadmID]) 
+    },
+    getAlarmItem: function (){
+        let _sql = `select * from D_ITEMS`
+        return allServices.query(_sql,[]) 
+    }
 }
 
 module.exports = allServices;
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.get('/', async (ctx, next) => {
   // ctx.body = await userService.findAllUser();
 })
 
+/*
 router.get('/HADMID', async (ctx, next) => {
   // arr.push(ctx.request.body['name']);
   // arr.push(ctx.request.body['pass']);
@@ -29,6 +30,68 @@ router.post('/HADMID', async (ctx, next) => {
   const res = await userService.getHADM_ID(subjectID);
   console.log(res);
   ctx.body = res;
+})
+*/
+
+function format(query) {
+  for(const key in query){
+    if(query.hasOwnProperty(key) && query[key]){
+      query[key] = parseInt(query[key])
+    }
+  }
+  return query;
+}
+// 病人基本信息
+router.get('/patient-info', async (ctx, next) => {
+  const {subjectID} = ctx.query;
+  console.log(ctx.query);
+  const res = await userService.getPatientInfo(parseInt(subjectID));
+  console.log(res);
+  ctx.body = res;
+})
+
+// 病人每次住院信息
+router.get('/admissions', async (ctx, next) => {
+  const {subjectID} = ctx.query;
+  console.log(ctx.query);
+  const res = await userService.getAdmissions(parseInt(subjectID));
+  console.log(res);
+  ctx.body = res;
+})
+
+// 病人用药信息
+router.get('/prescriptions', async (ctx, next) => {
+  const {subjectID} = ctx.query;
+  console.log(ctx.query);
+  const res = await userService.getPrescriptions(parseInt(subjectID));
+  console.log(res);
+  ctx.body = res;
+})
+
+// 病人化验信息
+router.get('/labEvents', async (ctx,next) =>{
+  const {subjectID ,hadmID} = ctx.query;
+  const res = await userService.getLabEvents(format({subjectID,hadmID}));
+  ctx.body = res;
+})
+
+// 病人chartEvents
+router.get('/chartEvents', async (ctx,next) =>{
+  const {subjectID ,hadmID} = ctx.query;
+  const res = await userService.getChartEvents(format({subjectID,hadmID}));
+  ctx.body = res;
+})
+
+// alarm d_item
+router.get('/alarmItem', async (ctx,next) =>{
+  const res = await userService.getAlarmItem();
+  const map = {};
+  for(const it of res){
+    if(it.CATEGORY && !map[it.CATEGORY]){
+      map[it.CATEGORY] = 1
+    }
+  }
+  ctx.body = map;
 })
 
 module.exports = router
